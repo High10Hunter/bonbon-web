0513c5dffc00aa95704f3a1bde2bdcbf51d2aea2
Trương Thoại Vỹ <115216420+ThoaiVy@users.noreply.github.com>
2024-11-19 15:53:15 +0700

    Feat/group (#6)
    * feat(group): crud group and fix personal finance
    
    * feat(group): crud group, crud event, crud item, crud spending
    
    * fix(group): fix spending update and create
    
    * fix(group): fix some features
    
    * fix:  resolve build errors

--- src/components/common/ConfirmModal.tsx
+++ src/components/common/ConfirmModal.tsx
@@ -1,10 +1,13 @@
-import { Modal } from 'antd'
+import { Modal, ModalFuncProps } from 'antd'
 import { forwardRef, useImperativeHandle, useState } from 'react'
 
 interface Props {
-  title: string
+  title?: string
   onOk?: () => void
   onCancel?: () => void
+  onDelete?: () => void
+  onEdit?: () => void
+  deleteAction?: ModalFuncProps
 }
 
 export interface IConfirmModalRef {
